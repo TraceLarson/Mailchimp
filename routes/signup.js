a857e2c50c070dd613e9ca63a1a9fdaabc43f028
Trace Larson <trace.larson4917@gmail.com>
2018-12-06 21:32:15 -0600

    added mailchimp data model to be send to server. Successfully adds new email to mail chimp list

--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 isInputEmpty = (req, res, next) => {
   //   const emptyValues = Object.keys(req.body).filter(value => !value);
@@ -10,7 +11,46 @@ isInputEmpty = (req, res, next) => {
 };
 
 router.post("/", isInputEmpty, (req, res, next) => {
-  req.root.length > 0 ? res.redirect("fail") : res.redirect("/success");
+  const { firstName, lastName, email } = req.body;
+
+  req.root.length > 0 ? res.redirect("fail") : "";
+
+  // Create request data
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const postData = JSON.stringify(data);
+
+  const options = {
+    url: "https://us19.api.mailchimp.com/3.0/lists/daaa0c6a2a",
+    method: "POST",
+    headers: {
+      Authorization: "auth 66d110a02a538d1d9beb9d3d3172c322-us19"
+    },
+    body: postData
+  };
+
+  request(options, (err, response, body) => {
+    if (err) {
+      res.redirect("/fail");
+    } else {
+      if (response.statusCode === 200) {
+        res.redirect("/success");
+      } else {
+        res.redirect("/fail");
+      }
+    }
+  });
 });
 
 module.exports = router;
